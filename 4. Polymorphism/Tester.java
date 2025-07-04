ab2b6c70af81cd5d01242d296ececdfb7588b80c
Rishabh <rishabh.jha0207@gmail.com>
2025-07-04 20:38:52 +0530

    Polymorphism Updated

---
+++ 4. Polymorphism/Tester.java
@@ -0,0 +1,27 @@
+public class Tester {
+    public static void main(String[] args) {
+
+        // PRACTICE
+        // GrandParent obj = new Parent();
+        // System.out.println();
+        // Parent obj2 = new Child();
+        // System.out.println();
+        // Child obj3 = new Child();
+
+
+        // QUESTION 1
+        
+        // Shape obj1 = new Rectangle(5.0,6.0);
+        // System.out.println(obj1.area(5.0,6.0));
+
+        // Circle obj2 = new Circle();
+        // System.out.println(obj2.area(7)); 
+
+
+        // Question 2
+
+        // Animal obj = new Animal();
+        // obj.
+
+    }
+}
