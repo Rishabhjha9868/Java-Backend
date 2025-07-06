9b61cd1e26b4127f2a8934b383941eb167f3f90d
Rishabh <rishabh.jha0207@gmail.com>
2025-07-06 21:32:33 +0530

    Project 2

--- Java Projects/1. Library Management System/Tester.java
+++ Java Projects/1. Library Management System/Tester.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("You want to Register as ");
+        System.out.println("Select your choice");
 
          // Scanner sc = new Scanner(System.in);
         // int num1 = sc.nextInt();
@@ -17,8 +14,8 @@ public class Tester {
         // System.out.println("Enter the second number");
         // int num2 = Integer.parseInt(br.readLine());
         // System.out.println("The sum is "+(num1+num2));
+        System.out.println("1. Register as ");
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
     }
     
