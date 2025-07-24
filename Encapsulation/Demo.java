b5dc2f030f473d11e5cbd370f2f05b756e4ffaed
Rishabh <rishabh.jha0207@gmail.com>
2025-07-24 21:43:08 +0530

    OOPS

---
+++ Encapsulation/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+public static void main(String[] args) {
+    Human obj = new Human();
+    obj.setage(25);
+    obj.setname("Rishabh Jha");
+    System.out.println("Name is " + obj.getname() + " and Age is " + obj.getage() );
+}
+    
+}
