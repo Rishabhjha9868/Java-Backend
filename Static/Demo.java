0364850cb5b6bf9f1103b913e1e14ca0602e28ee
Rishabh <rishabh.jha0207@gmail.com>
2025-07-24 11:35:58 +0530

    Static variable, method and block

--- Static/Demo.java
+++ Static/Demo.java
@@ -1,38 +1,22 @@
- class Mobile {
-    String brand;
-    String name;
-     static int price;
-
-    public void print(){
-        System.out.println("Brand name is " + brand + " Type is " + name + " Price is " + price + "$");
-    }
-
-    public static void mob (Mobile mob2){
-        System.out.println( "Brand name is " + mob2.brand + " Type is " + mob2.name + " Price is " + price + "$");
-    }
-}
-
+// Demo.java
 public class Demo {
     public static void main(String[] args) {
 
         // Object 1
-        Mobile mob = new Mobile();
-        
-        mob.brand = "Apple";
-        mob.name = "Smart Phone";
-         mob.price = 1500;
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Apple";
+        obj1.name = "Smart Phone";
+        obj1.price = 1500;
 
         // Object 2
-        Mobile mob2 = new Mobile();
-            mob2.brand = "Samsung";
-            mob2.name = "Smart Phone";
-            mob2.price = 1600;
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.name = "Smart Phone";
+        obj2.price = 1600;
 
-        mob.print();
-        mob2.print();
-       Mobile.mob(mob2);
-
+        obj1.print();
+        obj2.print();
+        // Mobile obj3 = new Mobile();
+        Mobile.mob(obj1);
     }
 }
-
-
