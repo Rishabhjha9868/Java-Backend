1b392fc708e0f3e7e59b5347507870b8843ff675
Rishabh <rishabh.jha0207@gmail.com>
2025-07-25 12:53:16 +0530

    Inheritence

---
+++ Inheritance/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        Calculator obj1 =new Calculator();
+        AdvanceCalc obj2 = new AdvanceCalc();
+      int r1=  obj2.Sub(5, 4);
+       int r2 =  obj2.add(5, 4);
+    System.out.println(r1 + " "+ r2);
+    }
+}
