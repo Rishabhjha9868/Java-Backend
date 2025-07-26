5b794c017f506dccbe7fb4400cbbfc7bdb8aca76
Rishabh <rishabh.jha0207@gmail.com>
2025-07-26 21:12:42 +0530

    Commit Super and this

--- Inheritance/Demo.java
+++ Inheritance/Demo.java
@@ -2,8 +2,12 @@ public class Demo {
     public static void main(String[] args) {
         Calculator obj1 =new Calculator();
         AdvanceCalc obj2 = new AdvanceCalc();
+        SuperComputer obj3 = new SuperComputer();
       int r1=  obj2.Sub(5, 4);
        int r2 =  obj2.add(5, 4);
-    System.out.println(r1 + " "+ r2);
+       int r3 = obj3.multi(5, 4);
+       int r4 = obj3.div(20, 4);
+       double r5 = obj3.pow(5, 4);
+    System.out.println(r1 + " "+ r2 + " "+  r3 + " " + r4 + " " + r5);
     }
 }
